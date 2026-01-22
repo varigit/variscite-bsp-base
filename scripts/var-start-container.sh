4cc5a1a744e1e70bc488875eeb12f56801f30672
FrancescoFerraro <francesco.f@variscite.com>
2026-01-22 09:32:10 +0100

    var-start-container: allow passing additional options
    Allow passing additional options directly to the var-start-container.sh script.
    
    Example: mount host /opt to store Yocto downloads:
     - ./var-start-container.sh -v /opt:/opt
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- scripts/var-start-container.sh
+++ scripts/var-start-container.sh
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-./sources/var-host-docker-containers/run.sh -u 22.04 -w "$WORKSPACE_DIR" -p
+./sources/var-host-docker-containers/run.sh -u 22.04 -w "$WORKSPACE_DIR" -p "$@"
 
 if [ $? -ne 0 ]; then
     echo "#################################################"
